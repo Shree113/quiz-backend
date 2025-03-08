768fee9d5fdad0e84260698a035bfa94aa64c8d8
Shree.A <shreecode05@gmail.com>
2025-03-09 02:06:53 +0530

    Added dj-database-url for Render deployment

--- quiz_backend/settings.py
+++ quiz_backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url  # Make sure this is installed
+import dj_database_url  # type: ignore # Make sure this is installed
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
